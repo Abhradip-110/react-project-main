19f4844f08c0c9f44e747c393b61e1a27a71bd4f
Abhradip-110 <abhra110@gmail.com>
2023-06-25 16:46:19 +0530

    "second publish"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+
+function Home() {
+  const[data,setData]=useState([])
+  const[records,setRecords]=useState([])
+
+  
+  const fetchData = async () => {
+    const res = await fetch("https://reqres.in/api/users?page=2");
+    const d = await res.json();
+    setData(d.data);
+    setRecords(d.data);
+
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const Filter =(event) =>  {
+    setRecords(data.filter( f  => f.first_name.toLowerCase().includes(event.target.value)
+      ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="p-5 bg-light">
+      <div className="bg-white shadow border">
+   
+         
+        <center> 
+          <h1>GREENDZINE TECHNOLOGIES React Assignment</h1>
+        <input type="text" className="search" onChange={Filter} placeholder="Search by FirstName" />
+        {records.map((dataObj, index) => {
+          return (
 
-export default App;
+            <div key={index} id={index} className="parent-div">
+            <div className="block">
+              <div className="icon">{dataObj.id}</div>
+              <img src={dataObj.avatar} alt="logo" className="image" />
+            </div>
+              <p style={{ fontSize: 20, color: 'black' }}>{dataObj.first_name}</p>
+            </div>
+          );
+        })}
+      </center>
+      </div>
+    </div>
+     
+  )
+
+}
+export default Home
